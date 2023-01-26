7883b4bfda9f04f7d33a88fda590df13d4a3ee0f
Sammy M <barsamms@gmail.com>
2023-01-26 14:50:19 +0300

    style: added prettier stlye checks

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ const app = express();
 
 app.use('/api/v1/users', userRoute);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT ?? 3000;
 
 const server = app.listen(PORT, () => {
 	console.log(`
